94f506ed2e98e700bdaf6e74063d2cab4eff48ca
kunallad54 <kunallad5473@gmail.com>
2021-06-10 17:27:50 +0530

    The start position of single player displayed

---
+++ SnakeLadderProblem.java
@@ -0,0 +1,19 @@
+//package com.company;
+
+/**
+ * The SnakeLadderProblem implements an application that
+ * displays initial position of player 1 in the game
+ *
+ * @author Krunal Lad
+ * @version 1.0
+ * @since 10-06-2021
+ */
+public class SnakeLadderProblem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake and Ladder Game!!!");
+        //Single Player
+        int startPosition= 0;
+        System.out.println("The initial position of a player is : "+startPosition);
+
+    }
+}
