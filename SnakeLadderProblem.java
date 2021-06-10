1ab13569167c2757374e6c8aa3e217797419fe70
kunallad54 <kunallad5473@gmail.com>
2021-06-10 19:11:38 +0530

    Dice Rumber Rolled till winning position 100 is reached

--- SnakeLadderProblem.java
+++ SnakeLadderProblem.java
@@ -15,12 +15,13 @@ public class SnakeLadderProblem {
         //Player position at start
         int START_POSITION= 0;
         int currentPosition = 0;
+	int WINNING_POSITION = 100;
         int NO_PLAY = 0;
         int LADDER = 1;
         int SNAKE = 2;
         System.out.println("The initial position of a player is : "+START_POSITION);
         //Just a random loop to get the work flow
-        for(int i =1;i<=10;i++){
+        while( currentPosition <= WINNING_POSITION ){
             int diceNumber = randomDiceNumber();
             int nextMove = NextMove();
             System.out.println("Random dice number generated is : "+diceNumber);
@@ -32,9 +33,10 @@ public class SnakeLadderProblem {
                 currentPosition = currentPosition + diceNumber;
             }else if (nextMove ==  SNAKE){
                 System.out.println("OOPS!!! ITS a SNAKE");
-                if(currentPosition != 0) {
-                    currentPosition = currentPosition - diceNumber;
-                }
+                currentPosition = currentPosition - diceNumber;
+		if(currentPosition < 0){
+			currentPosition = 0;
+		}
             }
             //printed in the loop to get count each time
             System.out.println("The current position of a player is: "+currentPosition);
