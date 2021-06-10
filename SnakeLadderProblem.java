fe06e51d34d3d3a7459159d344ce9d50415fcb07
kunallad54 <kunallad5473@gmail.com>
2021-06-10 19:39:30 +0530

    Ensures the winning position is exact 100

--- SnakeLadderProblem.java
+++ SnakeLadderProblem.java
@@ -5,8 +5,10 @@ import java.util.concurrent.ThreadLocalRandom;
  * displays initial position of player 1 in the game
  * Rolls a dice and gives random number between 1 to 6
  * Then player checks for options whether its no play,ladder or snake and perform actions
+ * Then dice is rolled till winning position 100 is reached
+ * Also Ensures that winning position not exceeds greater than 100
  * @author Krunal Lad
- * @version 1.2
+ * @version 1.4
  * @since 10-06-2021
  */
 public class SnakeLadderProblem {
@@ -14,14 +16,14 @@ public class SnakeLadderProblem {
         System.out.println("Welcome to Snake and Ladder Game!!!");
         //Player position at start
         int START_POSITION= 0;
+        int WINNING_POSITION = 100;
         int currentPosition = 0;
-	int WINNING_POSITION = 100;
         int NO_PLAY = 0;
         int LADDER = 1;
         int SNAKE = 2;
         System.out.println("The initial position of a player is : "+START_POSITION);
         //Just a random loop to get the work flow
-        while( currentPosition <= WINNING_POSITION ){
+        while(currentPosition <= WINNING_POSITION){
             int diceNumber = randomDiceNumber();
             int nextMove = NextMove();
             System.out.println("Random dice number generated is : "+diceNumber);
@@ -34,11 +36,17 @@ public class SnakeLadderProblem {
             }else if (nextMove ==  SNAKE){
                 System.out.println("OOPS!!! ITS a SNAKE");
                 currentPosition = currentPosition - diceNumber;
-		if(currentPosition < 0){
-			currentPosition = 0;
-		}
+                if(currentPosition <0) {
+                    currentPosition = 0;
+                }
             }
             //printed in the loop to get count each time
+            if (currentPosition > WINNING_POSITION) {
+                currentPosition = currentPosition - diceNumber;
+            }else if (currentPosition == WINNING_POSITION){
+                System.out.println("The current position of a player is: "+currentPosition);
+                break;
+            }
             System.out.println("The current position of a player is: "+currentPosition);
 
         }
@@ -56,4 +64,3 @@ public class SnakeLadderProblem {
         return (int) (Math.floor(Math.random() * 10)) % 3;
     }
 }
-
