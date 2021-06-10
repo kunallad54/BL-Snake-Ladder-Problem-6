9d3fb6602a7fad92569ef6f3b50715e1608932c1
kunallad54 <kunallad5473@gmail.com>
2021-06-10 18:52:27 +0530

    Checking options for No Play,Snake and Ladder added

--- SnakeLadderProblem.java
+++ SnakeLadderProblem.java
@@ -4,23 +4,54 @@ import java.util.concurrent.ThreadLocalRandom;
  * The SnakeLadderProblem implements an application that
  * displays initial position of player 1 in the game
  * Rolls a dice and gives random number between 1 to 6
+ * Then player checks for options whether its no play,ladder or snake and perform actions
  * @author Krunal Lad
- * @version 1.1
+ * @version 1.2
  * @since 10-06-2021
  */
 public class SnakeLadderProblem {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Game!!!");
         //Player position at start
-        int startPosition= 0;
-        // dice value is between 1 and 6 
+        int START_POSITION= 0;
+        int currentPosition = 0;
+        int NO_PLAY = 0;
+        int LADDER = 1;
+        int SNAKE = 2;
+        System.out.println("The initial position of a player is : "+START_POSITION);
+        //Just a random loop to get the work flow
+        for(int i =1;i<=10;i++){
+            int diceNumber = randomDiceNumber();
+            int nextMove = NextMove();
+            System.out.println("Random dice number generated is : "+diceNumber);
+            //The Player Checks for option whether its no play,ladder or snake and perform actions
+            if(nextMove == NO_PLAY){
+                System.out.println("Sorry you can not Play,You are at the Same Position");
+            }else if(nextMove == LADDER){
+                System.out.println("CONGRATS!!! You have got the ladder");
+                currentPosition = currentPosition + diceNumber;
+            }else if (nextMove ==  SNAKE){
+                System.out.println("OOPS!!! ITS a SNAKE");
+                if(currentPosition != 0) {
+                    currentPosition = currentPosition - diceNumber;
+                }
+            }
+            //printed in the loop to get count each time
+            System.out.println("The current position of a player is: "+currentPosition);
+
+        }
+
+    }
+    static int randomDiceNumber(){
+        // dice value is between 1 and 6
         int MIN_DICE_VALUE = 1;
         int MAX_DICE_VALUE = 6;
         //nextInt is exclusive of top value so added plus 1
-        int randomNum = ThreadLocalRandom.current().nextInt(MIN_DICE_VALUE, MAX_DICE_VALUE + 1);
-        System.out.println("The initial position of a player is : "+startPosition);
-        System.out.println("After rolling a die position of player is at : "+randomNum);
-
+        return ThreadLocalRandom.current().nextInt(MIN_DICE_VALUE, MAX_DICE_VALUE + 1);
+    }
+    static int NextMove(){
+        //generating random numbers 0,1,2 for no play,ladder,snake
+        return (int) (Math.floor(Math.random() * 10)) % 3;
     }
 }
 
